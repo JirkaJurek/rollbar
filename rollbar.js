ecef6b29b453ed3261e1bb1ddf0edd842517c76a
Jiří Jurek <jirka.jurek@gmail.com>
2025-02-23 13:45:43 +0100

    Create rollbar.js

---
+++ rollbar.js
@@ -0,0 +1,17 @@
+const xhttp = new XMLHttpRequest();
+xhttp.onload = function() {
+  var x = JSON.parse(this.responseText);
+  let bar_text = "";
+  //'Meta Data', 'Time Series (Daily)'
+  var ts = x['Time Series (Daily)'];
+  for (t in ts) {
+    bar_text += t + ": ";
+    var tsi = ts[t];
+    for ( l in tsi ) {
+      bar_text += "<a href='https://www.google.com'>" + l + ": " + tsi[l];
+    }  
+  }
+  document.getElementById('sdata').innerHTML = bar_text + ' ' + bar_text;
+};
+xhttp.open("GET", "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=YOUR_API_KEY");
+xhttp.send();
